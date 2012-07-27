3517ea551de66e5b69bd2cb06fb3bfbea86d045c
Qingyu Chen <s3257935@yallara.cs.rmit.edu.au>
2012-07-27 15:25:16 +1000

    added connect script

---
+++ db.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Define the DB connection details separately so that 
+ * they are not publicly available in github. 
+ *
+ */
+
+/**
+ * Hostname and port mysql is running on (can't use localhost)
+ */
+define('DB_HOST',   'yallara.cs.rmit.edu.au:54711');
+/**
+ * Name of database to connect to
+ */
+define('DB_NAME',   'winestore');
+/**
+ * Username to connect with
+ */
+define('DB_USER',   'S3257935');
+
+/**
+ * Password to connect with
+ */
+define('DB_PW',     '123');
+?>
